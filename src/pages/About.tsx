2098007e855878c3e159ab4fed169462bd3c6d67
Nazar Artemenkov <bima62374@gmail.com>
2025-11-02 16:22:19 +0200

    chenge email in about

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -126,8 +126,8 @@ export default function About() {
           <p className="about-footer__note">
             Write to us, tell us about your current workflow, and get personal onboarding as soon as we open the doors.
           </p>
-          <a className="about-footer__link" href="mailto:team@10steel.io">
-            team@10steel.io
+          <a className="about-footer__link" href="">
+            ToDoBeDo@tdbd.com
           </a>
         </footer>
       </section>
